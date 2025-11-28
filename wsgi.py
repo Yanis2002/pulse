20829d6d86997782aa4b769b2c5b4166b96b1601
Yanis2002 <bendr3656@gmail.com>
2025-11-28 19:08:07 +0300

    Fix WSGI application export

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,9 @@ WSGI entry point for Amvera deployment
 import os
 from app import app, socketio
 
+# Export app and socketio for WSGI servers
+application = app
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
