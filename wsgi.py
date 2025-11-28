bf038f678bbf37a7d6f09e3a53f1d0c243c54789
Yanis2002 <bendr3656@gmail.com>
2025-11-28 19:07:54 +0300

    Add WSGI entry point and fix routes for Amvera deployment

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+"""
+WSGI entry point for Amvera deployment
+"""
+import os
+from app import app, socketio
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "8000"))
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
